ea9d456d4774ee9778c30f86b5965c5302648abb
Katalyst99 <katleho333@gmail.com>
2024-06-25 01:54:19 -0700

    SQLAlchemy state

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String,
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete-orphan",
+                              backref="state")
+    else:
+        @property
+        def cities(self):
+            """Gettter atribute that returns City objects"""
+            cities_obj = []
+            for city_val in models.storage.all(City).values():
+                if city_val.state_id == self.id:
+                    cities_obj.append(city_val)
+            return cities_obj
+
+
+
