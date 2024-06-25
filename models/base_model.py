9652cc361c70c6243dab586f3fec69530cfb4b35
Katalyst99 <katleho333@gmail.com>
2024-06-24 23:48:05 -0700

    SQLAlchemy to base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -14,33 +14,33 @@ class BaseModel:
     id = Column(String(60), unique=True, primary_key=True, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs:
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    dt = "%Y-%m-%dT%H:%M:%S.%f"
+                    val = datetime.strptime(kwargs[key], dt)
+                elif key != "__class__":
+                    setattr(self, key, val)
 
     def __str__(self):
         """Returns a string representation of the instance"""
         # cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        instance_dict = self.__dict__.copy()
+        instance_dict.pop('_sa_instance_state', None)
         return '[{}] ({}) {}'.format(self.__class__.__name__, self.id,
-                                     self.__dict__)
+                                     instance_dict)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -51,4 +51,9 @@ class BaseModel:
         dictionary['__class__'] = self.__class__.__name__
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary.pop('_sa_instance_state', None)
         return dictionary
+
+    def delete(self):
+        """deletes the current instance from the storage"""
+        models.storage.delete(self)
