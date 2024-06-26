935d4d031e1e5244d341cf861860612b2dac7979
Katalyst99 <katleho333@gmail.com>
2024-06-26 02:34:29 -0700

    DBStorage - Amenity... and BOOM!

--- models/place.py
+++ models/place.py
@@ -3,8 +3,19 @@
 from os import getenv
 from models.base_model import BaseModel, Base
 from models.review import Review
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
+import models
+
+if getenv('HBNB_TYPE_STORAGE') == "db":
+    metadata = Base.metadata
+    place_amenity = Table('place_amenity', metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'), primary_key=True,
+                                 nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'), primary_key=True,
+                                 nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -21,9 +32,12 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float)
         longitude = Column(Float)
+        amenity_ids = []
         reviews = relationship("Review", cascade="all, delete-orphan",
                                backref="place")
-
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False,
+                                 backref="places")
     else:
         city_id = ""
         user_id = ""
@@ -45,3 +59,21 @@ class Place(BaseModel, Base):
                 if review_val.place_id == self.id:
                     reviews_list.append(review_val)
             return reviews_list
+
+        @property
+        def amenities(self):
+            """Gettter atribute that returns the list of  instances"""
+            amen_list = []
+            for amenity in models.storage.all(Amenity).values():
+                if amenity.id in self.amenity_ids:
+                    amen_list.append(amenity)
+            return amen_list
+
+        @amenities.setter
+        def amenities(self, am_obj):
+            """
+            Setter attributethat handles append method for
+            adding an Amenity.id to the attribute amenity_ids
+            """
+            if type(am_obj) == Amenity:
+                self.amenity_ids.append(am_obj.id)
